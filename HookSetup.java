781f9156f9042caa016028de18c34d576eb5cbfd
Vydehi-Mamillapalli <90857675+Vydehi-Mamillapalli@users.noreply.github.com>
2021-12-08 02:42:49 +0000

    Add files via upload

---
+++ HookSetup.java
@@ -0,0 +1,79 @@
+package example.hooksetup;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.enums.Browsers;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class HookSetup {
+    public static WebDriver driver;
+    public static Properties prop;
+    private static final Logger log = LogManager.getLogger(HookSetup.class);
+
+    public HookSetup()  {
+        prop = new Properties();
+        FileInputStream ip = null;
+        try {
+            ip = new FileInputStream(System.getProperty("user.dir")+ "/src/main/java/org/example"
+                    + "/config/config.properties");
+            prop.load(ip);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    protected WebDriverWait driverWait(long timeoutSeconds) {
+        return new WebDriverWait(driver, timeoutSeconds);
+    }
+
+    public WebDriver browserSetup() throws Exception {
+        log.info("++++++Before hooks+++++++");
+
+        String browserName = prop.getProperty("browser");
+
+        if(browserName.equalsIgnoreCase(Browsers.CHROME.name())){
+            ChromeOptions chromeOptions = new ChromeOptions();
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(chromeOptions);
+        }
+        else if(browserName.equalsIgnoreCase(Browsers.FIREFOX.name())){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        else if(browserName.equalsIgnoreCase(Browsers.EDGE.name())){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        else{
+            //If no browser passed throw exception
+            throw new Exception("Browser is not correct");
+        }
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+
+
+
+}
